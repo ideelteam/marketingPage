fe161ea849e1527be6768045be83503498224e65
Mason Kam <kamm1@seattleu.edu>
2017-03-08 22:53:16 -0800

    First Commit Template

---
+++ all/modules/core/controllers/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name To Do App
+ * @description
+ * # To do Application
+ *
+ * Main module of the application.
+ */
+angular
+    .module('iDeel', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'modules/core/views/welcome.html'
+            })
+            .when('/about_us', {
+                templateUrl: 'modules/core/views/about_us.html'
+            })
+            .when('/send_comments', {
+                templateUrl: 'modules/comments/views/send_comments.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
